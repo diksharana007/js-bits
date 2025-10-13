53fbf429bd2d52ad84390c6b910c4be576f60932
diksharana007/js-bits <diksharana1215@gmail.com>
2025-10-13 23:23:49 +0530

    Added CountDown-timer project

---
+++ CountDown-timer/script.js
@@ -0,0 +1,30 @@
+const days =document.getElementById('days');
+const hours =document.getElementById('hours');
+const mins =document.getElementById('mins');
+const secs =document.getElementById('secs');
+
+ const formatTime =(time) =>{
+        return time < 10 ? `0${time}` : time;
+     }
+
+const updateCountDown =(deadline) => {
+    const currentTime = new Date();
+    const timeDifference = deadline - currentTime; // milliseconds
+
+    // calculate days,hours,mins,secs from timeDifference
+    let calcSecs = Math.floor(timeDifference/1000) % 60;
+    let calcMins = Math.floor(timeDifference/1000/60) % 60;
+    let calcHours = Math.floor(timeDifference/1000/60/60) % 24;
+    let calcDays = Math.floor(timeDifference/1000/60/60/24);
+
+    days.textContent=formatTime(calcDays);
+    mins.textContent=formatTime(calcMins);
+    hours.textContent=formatTime(calcHours);
+    secs.textContent=formatTime(calcSecs);
+
+}
+const countDown = (targetDate) => {
+    setInterval(() => updateCountDown(targetDate),1000);
+}
+const targetDate=new Date("November 04 2025 07:00");
+countDown(targetDate);
